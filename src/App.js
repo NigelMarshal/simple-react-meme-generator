91b74a71a5fdc34aae82ab5909fee6afa0deec8f
Nigel Marshal <nigelmarshal@gmail.com>
2021-04-05 11:05:30 +0400

    styles(main): Some cleanup and moving styles from code sandbox playground

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
+import React from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state ={}
+    }
+    
+    render() {
+        return (
+            <h1>Dank Memes</h1>
+        )
+    }
+}
+
+export default MemeGenerator
